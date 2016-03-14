99d880904af4caeb31f232c0ceaafd9ee00353ac
xingxuewei <xingxuewei@jd.com>
2016-03-14 12:23:57 +0800

    Initial commit

---
+++ Example/podTestLibrary/PTLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTLAppDelegate.h
+//  podTestLibrary
+//
+//  Created by xingxuewei on 03/14/2016.
+//  Copyright (c) 2016 xingxuewei. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PTLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/podTestLibrary/PTLViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTLViewController.h
+//  podTestLibrary
+//
+//  Created by xingxuewei on 03/14/2016.
+//  Copyright (c) 2016 xingxuewei. All rights reserved.
+//
+
+@import UIKit;
+
+@interface PTLViewController : UIViewController
+
+@end
